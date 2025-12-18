32e01423a15efa754d98cc86065e5f2c0e09d948
zhangxy <zhangxy02@shinaisports.com>
2025-12-18 18:09:53 +0800

    修改样式

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,31 @@
-'use client'
+'use client';
 
-import { useState } from "react";
+import { useState } from 'react';
+import EastIcon from '@mui/icons-material/East';
 
 export default function Home() {
-	const [input, setInput] = useState("");
-	const [model, setModel] = useState("deepseek-v3");
-  return (
-    <div className="h-screen flex  flex-col items-center">
-      <div className="h-1/5"></div>
-	   <div className="h-1/2">
-		<p className="text-bold text-2xl text-center">有什么可以帮您的吗</p>
+	const [input, setInput] = useState('');
+	const [model, setModel] = useState('deepseek-v3');
+	return (
+		<div className='h-screen flex  flex-col items-center'>
+			<div className='h-1/5'></div>
+			<div className='w-1/2'>
+				<p className='text-bold text-2xl text-center'>有什么可以帮您的吗</p>
 
-		<div className="flex flex-col  items-center justify-center mt-4 shadow-lg border-[1px] border-gray-300 h-32 rounded-lg">
-		<textarea className="w-full  rounded-lg p-3 h-30 focus:outline-none" value={input} onChange={(e) => setInput(e.target.value)}></textarea>
-		<div className="flex flex-row items-center justify-center w-full h-12 mb-2">
-			<select className="w-1/3 rounded-lg p-3 h-10 focus:outline-none" value={model} onChange={(e) => setModel(e.target.value)}>
-				<option value="deepseek-v3">deepseek-v3</option>
-				<option value="deepseek-v2">deepseek-v2</option>
-			</select>
+				<div className='flex flex-col items-center justify-center mt-4 shadow-lg border border-gray-300 h-32 rounded-lg'>
+					<textarea className='w-full rounded-lg p-3 h-30 focus:outline-none' value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+					<div className='flex flex-row items-center justify-between w-full h-12 mb-2'>
+						<div>
+							<div className={`flex flex-row items-center justify-center rounded-lg border px-2 py-1 ml-2 cursor-pointer ${model === 'deepseek-r1' ? 'border-blue-300 bg-blue-200' : 'border-gray-300'} `}>
+								<p className='text-sm'>深度思考（R1）</p>
+							</div>
+						</div>
+						<div className='flex items-center justify-center border-2 mr-4 border-black p-1 rounded-full'>
+							<EastIcon />
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
-	   </div>
-    </div>
-  );
+	);
 }
