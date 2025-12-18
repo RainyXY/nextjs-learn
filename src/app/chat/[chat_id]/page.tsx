0d5a1e44ed012af3c424e67cd2142eb6405966dc
RainyXY <shellkxy@126.com>
2025-12-18 23:39:46 +0800

    更新

--- src/app/chat/[chat_id]/page.tsx
+++ src/app/chat/[chat_id]/page.tsx
@@ -1,39 +1,36 @@
 'use client';
 
+import { useRef, useEffect } from 'react';
 import { useChat } from '@ai-sdk/react';
-import { DefaultChatTransport } from 'ai';
-import { useState } from 'react';
 
 export default function Page() {
-	const { messages, sendMessage, status } = useChat({
-		transport: new DefaultChatTransport({
-			api: '/api/chat',
-		}),
-	});
-	const [input, setInput] = useState('');
+	const { messages, input, handleInputChange, handleSubmit } = useChat({});
+
+	const endRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (endRef?.current) {
+			endRef.current?.scrollIntoView({ behavior: 'smooth' });
+		}
+	}, [messages]);
 
 	return (
-		<>
-			{messages.map((message) => (
-				<div key={message.id}>
-					{message.role === 'user' ? 'User: ' : 'AI: '}
-					{message.parts.map((part, index) => (part.type === 'text' ? <span key={index}>{part.text}</span> : null))}
+		<div className='flex  flex-col justify-between items-center h-screen'>
+			<div className='flex flex-col justify-between w-2/3 gap-8 overflow-y-auto flex-1'>
+				<div className='h-4'></div>
+				<div className='flex flex-col gap-8 flex-1'>
+					{messages?.map((message) => (
+						<div key={message.id} className={`rounded-lg flex flex-row  ${message?.role === 'assistant' ? 'justify-start mr-18' : 'justify-end ml-10'}`}>
+							<p className={`inline-block p-2 rounded-lg ${message?.role === 'assistant' ? 'bg-blue-300' : 'bg-slate-100'}`}>{message?.content}</p>
+						</div>
+					))}
 				</div>
-			))}
+			</div>
 
-			<form
-				onSubmit={(e) => {
-					e.preventDefault();
-					if (input.trim()) {
-						sendMessage({ text: input });
-						setInput('');
-					}
-				}}>
-				<input value={input} onChange={(e) => setInput(e.target.value)} disabled={status !== 'ready'} placeholder='Say something...' />
-				<button type='submit' disabled={status !== 'ready'}>
-					Submit
-				</button>
+			<form onSubmit={handleSubmit}>
+				<input name='prompt' value={input} onChange={handleInputChange} />
+				<button type='submit'>Submit</button>
 			</form>
-		</>
+		</div>
 	);
 }
