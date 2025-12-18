527fd1c0294028e44ab61c48c11884839c04b26e
RainyXY <shellkxy@126.com>
2025-12-18 23:39:46 +0800

    聊天页

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 'use client';
 
 import { useState } from 'react';
+import { useChat } from '@ai-sdk/react';
 import EastIcon from '@mui/icons-material/East';
 
 export default function Home() {
 	const [input, setInput] = useState('');
 	const [model, setModel] = useState('deepseek-v3');
+	const { handleSubmit } = useChat({});
+	const handleChangeModel = () => {
+		setModel(model === 'deepseek-v3' ? 'deepseek-r1' : 'deepseek-v3');
+	};
 	return (
 		<div className='h-screen flex  flex-col items-center'>
 			<div className='h-1/5'></div>
@@ -16,11 +21,13 @@ export default function Home() {
 					<textarea className='w-full rounded-lg p-3 h-30 focus:outline-none' value={input} onChange={(e) => setInput(e.target.value)}></textarea>
 					<div className='flex flex-row items-center justify-between w-full h-12 mb-2'>
 						<div>
-							<div className={`flex flex-row items-center justify-center rounded-lg border px-2 py-1 ml-2 cursor-pointer ${model === 'deepseek-r1' ? 'border-blue-300 bg-blue-200' : 'border-gray-300'} `}>
-								<p className='text-sm'>深度思考（R1）</p>
+							<div
+								className={`flex flex-row items-center justify-center rounded-lg border px-2 py-1 ml-2 cursor-pointer ${model === 'deepseek-r1' ? 'border-blue-300 bg-blue-200' : 'border-gray-300'} `}
+								onClick={handleChangeModel}>
+								<p className='text-sm'>{model}</p>
 							</div>
 						</div>
-						<div className='flex items-center justify-center border-2 mr-4 border-black p-1 rounded-full'>
+						<div className='flex items-center justify-center border-2 mr-4 border-black p-1 rounded-full' onClick={handleSubmit}>
 							<EastIcon />
 						</div>
 					</div>
